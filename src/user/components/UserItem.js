80afa228a048f53d14314b93db52787af510aaa2
Hannah Z <eveline1129@163.com>
2024-08-30 17:16:03 -0500

    added avatar

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,14 +1,18 @@
 import React from "react"
 import "./UserItem.css"
+import Avatar from "../../shared/components/UIElements/Avatar"
+import { Link } from "react-router-dom"
 const UserItem = (props) => {
 	return (
 		<li className='user-item'>
 			<div className='user-item__content'>
 				<div className='user-item__image'>
-					<img
-						src={props.image}
-						alt={props.name}
-					/>
+					<Link to={`/${props.id}/places`}>
+						<Avatar
+							image={props.image}
+							alt={props.name}
+						/>
+					</Link>
 				</div>
 				<div className='user-item__info'>
 					<h2>{props.name}</h2>
