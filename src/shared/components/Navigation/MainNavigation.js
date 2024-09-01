986cc59b5029d9e506a240174b9ab1e27f497218
Hannah Z <eveline1129@163.com>
2024-08-31 23:05:31 -0500

    render side drawer using portal

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -4,22 +4,32 @@ import { Link } from "react-router-dom"
 import MainHeader from "./MainHeader"
 import "./MainNavigation.css"
 import NavLinks from "./NavLinks"
+import SideDrawer from "./SideDrawer"
 
 const MainNavigation = (props) => {
 	return (
-		<MainHeader>
-			<button className='main-navigation__menu-btn'>
-				<span />
-				<span />
-				<span />
-			</button>
-			<h1 className='main-navigation__title'>
-				<Link to='/'>YourPlaces</Link>
-			</h1>
-			<nav>
-				<NavLinks />
-			</nav>
-		</MainHeader>
+		<React.Fragment>
+			<SideDrawer>
+				<nav className='main-navigation__drawer-nav'>
+					<NavLinks />
+				</nav>
+			</SideDrawer>
+			<MainHeader>
+				<button className='main-navigation__menu-btn'>
+					<span />
+					<span />
+					<span />
+				</button>
+				<h1 className='main-navigation__title'>
+					<Link to='/'>YourPlaces</Link>
+				</h1>
+				<nav>
+					<nav className='main-navigation__header-nav'>
+						<NavLinks />
+					</nav>
+				</nav>
+			</MainHeader>
+		</React.Fragment>
 	)
 }
 
