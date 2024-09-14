9e4e94e2b96c052331201b7b583c4f273f0d9f40
Hannah Z <eveline1129@163.com>
2024-09-14 15:47:57 -0500

    Added AuthContext

--- src/App.js
+++ src/App.js
@@ -11,45 +11,57 @@ import MainNavigation from "./shared/components/Navigation/MainNavigation"
 import UserPlaces from "./places/pages/UserPlaces"
 import UpdatePlace from "./places/pages/UpdatePlace"
 import Auth from "./user/pages/Auth"
+import { AuthContext } from "./shared/context/auth-context"
+import { useCallback, useState } from "react"
 
 const App = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState()
+	const login = useCallback(() => {
+		setIsLoggedIn(true)
+	}, [])
+	const logout = useCallback(() => {
+		setIsLoggedIn(false)
+	}, [])
 	return (
-		<Router>
-			<MainNavigation />
-			<main>
-				<Switch>
-					<Route
-						path='/'
-						exact
-					>
-						<Users />
-					</Route>
-					<Route
-						path='/auth'
-						exact
-					>
-						<Auth />
-					</Route>
-					<Route path='/:userId/places'>
-						<UserPlaces />
-					</Route>
+		//  whenever any of these values changes, this will all be passed down to all the child component
+		<AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+			<Router>
+				<MainNavigation />
+				<main>
+					<Switch>
+						<Route
+							path='/'
+							exact
+						>
+							<Users />
+						</Route>
+						<Route
+							path='/auth'
+							exact
+						>
+							<Auth />
+						</Route>
+						<Route path='/:userId/places'>
+							<UserPlaces />
+						</Route>
 
-					<Route
-						path='/places/new'
-						exact
-					>
-						<NewPlace />
-					</Route>
-					<Route
-						path='/places/:placeId'
-						exact
-					>
-						<UpdatePlace />
-					</Route>
-					<Redirect to='/' />
-				</Switch>
-			</main>
-		</Router>
+						<Route
+							path='/places/new'
+							exact
+						>
+							<NewPlace />
+						</Route>
+						<Route
+							path='/places/:placeId'
+							exact
+						>
+							<UpdatePlace />
+						</Route>
+						<Redirect to='/' />
+					</Switch>
+				</main>
+			</Router>
+		</AuthContext.Provider>
 	)
 }
 
