f95eaf4be2667f2997e142af1423eb8a298d4448
Hannah Z <eveline1129@163.com>
2024-09-13 23:52:38 -0500

    tested no data

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -12,32 +12,32 @@ import { useForm } from "../../shared/hooks/form-hook"
 import Card from "../../shared/components/UIElements/Card"
 
 const DUMMY_PLACES = [
-	{
-		id: "p1",
-		title: "Empire State Building",
-		description: "One of the most famous sky scrapers in the world!",
-		imageUrl:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-		address: "20 W 34th St, New York, NY 10001",
-		location: {
-			lat: 40.7484405,
-			lng: -73.9878584,
-		},
-		creator: "u1",
-	},
-	{
-		id: "p2",
-		title: "Empire State Building",
-		description: "One of the most famous sky scrapers in the world!",
-		imageUrl:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-		address: "20 W 34th St, New York, NY 10001",
-		location: {
-			lat: 40.7484405,
-			lng: -73.9878584,
-		},
-		creator: "u2",
-	},
+	// {
+	// 	id: "p1",
+	// 	title: "Empire State Building",
+	// 	description: "One of the most famous sky scrapers in the world!",
+	// 	imageUrl:
+	// 		"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+	// 	address: "20 W 34th St, New York, NY 10001",
+	// 	location: {
+	// 		lat: 40.7484405,
+	// 		lng: -73.9878584,
+	// 	},
+	// 	creator: "u1",
+	// },
+	// {
+	// 	id: "p2",
+	// 	title: "Empire State Building",
+	// 	description: "One of the most famous sky scrapers in the world!",
+	// 	imageUrl:
+	// 		"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+	// 	address: "20 W 34th St, New York, NY 10001",
+	// 	location: {
+	// 		lat: 40.7484405,
+	// 		lng: -73.9878584,
+	// 	},
+	// 	creator: "u2",
+	// },
 ]
 
 const UpdatePlace = () => {
